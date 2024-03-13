a9fb42f02c35a480463c20bacd81affd81b0986f
paigepreziosi <pvpreziosi@hotmail.com>
2024-03-13 11:58:48 -0400

    commit index

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,10 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 
+router.use('/dashboard', dashboardRoutes);
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+
+module.exports = router;
 
 module.exports = router;
